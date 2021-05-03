b761644884a4a7182f479f6d123d8c1780190c37
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2021-05-03 19:51:04 +0430

    add collection item component

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,16 @@
+import "./collection-item.styles.css";
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
+    <div className="collection-footer">
+      <div className="name">{name}</div>
+      <div className="price">{price}</div>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,11 +1,14 @@
+import CollectionItem from "../collection-item/collection-item.component";
+import "./collection-preview.styles.css";
+
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h2 className="title">{title.toUpperCase()}</h2>
     <div className="preview">
       {items
         .filter((item, index) => index < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherProps }) => (
+          <CollectionItem key={id} {...otherProps} />
         ))}
     </div>
   </div>

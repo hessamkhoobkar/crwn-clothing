749d70aafd7d7e1e648da306299e85e162f495b9
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2021-05-05 11:16:15 +0430

    add custom button

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,9 @@
+import "./custom-button.style.css";
+
+const CustomButton = ({ children, ...otherProps }) => (
+  <button className="custom-button" {...otherProps}>
+    {children}
+  </button>
+);
+
+export default CustomButton;

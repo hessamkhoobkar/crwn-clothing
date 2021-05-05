44e866f5bae199f013c66b5ec171f3ee5f302ebe
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2021-05-05 11:19:05 +0430

    import AuthPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/homepage/homepage.component";
+import AuthPage from "./pages/auth-page/auth-page.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={AuthPage} />
       </Switch>
     </div>
   );

c487da90064b44fd44ca524d5120b6829a9e806a
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2021-05-05 11:16:02 +0430

    add auth page

---
+++ src/pages/auth-page/auth-page.component.jsx
@@ -0,0 +1,11 @@
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./auth-page.style.css";
+
+const AuthPage = () => (
+  <div className="auth-page">
+    <h1>Auth Page</h1>
+    <SignIn />
+  </div>
+);
+
+export default AuthPage;
